c43c0b685772ea80f89b995da318baf37b34e07c
Vane2005 <vanessaduranmona@gmail.com>
2026-04-17 18:29:34 -0500

    Envia estructura base del proyecto

---
+++ src/main/java/co/edu/univalle/vivaeventosorderservice/VivaeventosOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventosorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaeventosOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivaeventosOrderServiceApplication.class, args);
+    }
+
+}
