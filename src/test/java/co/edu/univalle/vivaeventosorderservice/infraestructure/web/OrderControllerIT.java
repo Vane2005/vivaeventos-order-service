c11711abb0ca5e338f20d5a8789961af15e098df
AlejandroGarz <alejand201@gmail.com>
2026-06-04 21:45:08 -0500

    feat: activate test profile in OrderControllerIT

--- src/test/java/co/edu/univalle/vivaeventosorderservice/infraestructure/web/OrderControllerIT.java
+++ src/test/java/co/edu/univalle/vivaeventosorderservice/infraestructure/web/OrderControllerIT.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 // @WebMvcTest carga SOLO la capa web — mucho más rápido que @SpringBootTest
 @WebMvcTest(OrderController.class)
+@ActiveProfiles("test")
 class OrderControllerIT {
 
     @Autowired
