98a983bd273f47b09b52cddd0b37ccd7008134dc
AlejandroGarz <alejand201@gmail.com>
2026-06-04 21:21:42 -0500

    feat: add unit and integration tests for order service

--- src/main/java/co/edu/univalle/vivaeventosorderservice/application/dto/DiscountCodeResponse.java
+++ src/main/java/co/edu/univalle/vivaeventosorderservice/application/dto/DiscountCodeResponse.java
@@ -1,10 +1,12 @@
 package co.edu.univalle.vivaeventosorderservice.application.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
 public class DiscountCodeResponse {
     private String code;
     private String discountType;
